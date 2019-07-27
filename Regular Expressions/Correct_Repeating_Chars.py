964983594e536a189225c818874eea7a5c71d821
Farooq Zafar <53262137+farooqzafarawan@users.noreply.github.com>
2019-07-27 10:25:54 +0500

    Create Correct_Repeating_Chars.py

---
+++ Regular Expressions/Correct_Repeating_Chars.py
@@ -0,0 +1,21 @@
+# Correcting Repeating Characters
+import re
+
+old_word = 'finalllyyy'
+repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
+match_substitution = r'\1\2\3'
+step = 1
+
+while True:
+    # remove one repeated character
+    new_word = repeat_pattern.sub(match_substitution,
+                                  old_word)
+    if new_word != old_word:
+         print('Step: {} Word: {}'.format(step, new_word))
+         step += 1 # update step
+         # update old word to last substituted state
+         old_word = new_word
+         continue
+    else:
+         print("Final word:", new_word)
+         break
