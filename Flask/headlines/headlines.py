e847b0fa5c9b0c02d3065a6c92f1dc434c8659aa
Farooq Zafar Awan <farooq.zafar@systemsltd.com>
2020-02-19 18:23:46 +0500

    articles=feed['entries'] was article=first_article

--- Flask/headlines/headlines.py
+++ Flask/headlines/headlines.py
@@ -23,7 +23,7 @@ def get_news(publication="fox"):
   feed = feedparser.parse(RSS_FEEDS[publication])
   first_article = feed['entries'][0]
 
-  return render_template("home.html", article=first_article)
+  return render_template("home.html", articles=feed['entries'])
 
 # if __name__ == "__main__":
 #   app.run(port=5002, debug=True)
