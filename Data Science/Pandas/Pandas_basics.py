2701a8c933bb4311731b192af19fa3df6c612625
Farooq Zafar Awan <farooq.zafar@systemsltd.com>
2020-02-25 19:46:54 +0500

    Add File

---
+++ Data Science/Pandas/Pandas_basics.py
@@ -0,0 +1,30 @@
+import requests
+import os 
+import pandas as pd
+
+download_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv"
+
+outDir = r'D:\CODE\PYSCRIPTS\PY_DATA'
+target_csv_path = os.path.join(outDir,"nba_all_elo.csv")
+
+# Download nba results in csv
+# response = requests.get(download_url)
+# response.raise_for_status()    # Check that the request was successful
+# with open(target_csv_path, "wb") as f:
+#     f.write(response.content)
+
+nba = pd.read_csv(target_csv_path)
+
+# Length of Data => 126314
+len(nba)
+
+# Dimensionality or shape results in a tuple containing the number of rows and columns.
+# (126314, 23)
+nba.shape
+
+# First 5 rows
+nba.head()
+
+# Set Display to all columns
+pd.set_option("display.max.columns", None)
+
