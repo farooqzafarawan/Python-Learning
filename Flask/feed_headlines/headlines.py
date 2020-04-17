0f7f296984c3da0d01197a944132c71c44a67727
Farooq Zafar <53262137+farooqzafarawan@users.noreply.github.com>
2020-04-17 19:22:00 +0500

    Create headlines.py

---
+++ Flask/feed_headlines/headlines.py
@@ -0,0 +1,27 @@
+import feedparser
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+RSS_FEEDS = {'yahoo': 'https://www.yahoo.com/news/rss/world',
+             'oreilly': 'http://feeds.feedburner.com/oreilly/radar/atom',
+             'google': 'https://news.google.com/rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest'
+            }
+
+
+@app.route("/")
+def get_news():
+  query = request.args.get("publication")
+  if not query or query.lower() not in RSS_FEEDS:
+          publication = "fox"
+  else:
+          publication = query.lower()
+
+  feed = feedparser.parse(RSS_FEEDS[publication])
+
+  return render_template("home.html", articles=feed['entries'])
+
+if __name__ == "__main__":
+   app.run(port=5002, debug=False)
