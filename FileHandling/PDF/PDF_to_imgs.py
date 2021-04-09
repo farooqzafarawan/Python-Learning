e36e70140656db78c3c7a7d0f4c8f6340158c1af
mohibalvi <mohib.tech@gmail.com>
2021-04-09 22:36:10 +0500

    New Code File

---
+++ FileHandling/PDF/PDF_to_imgs.py
@@ -0,0 +1,42 @@
+from PyPDF4 import PdfFileReader,PdfFileWriter
+from os.path import join
+#import PyPDF2
+from PIL import Image
+
+workDir = r'C:\URDU\eBooks\PDF books\MISC'
+inFile = 'Laal Qilay ky Shaam-e-Sahar' + '.pdf'
+filePDF = join(workDir, inFile)
+
+def extractImages():
+    pdfReader = PdfFileReader(open(filePDF, "rb"))
+
+    for pgNum in range(pdfReader.numPages):
+        page = pdfReader.getPage(pgNum)
+        xObject = page['/Resources']['/XObject'].getObject()
+
+        for obj in xObject:
+            if xObject[obj]['/Subtype'] == '/Image':
+                size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+                data = xObject[obj].getData()
+                if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                    mode = "RGB"
+                else:
+                    mode = "P"
+
+                if xObject[obj]['/Filter'] == '/FlateDecode':
+                    img = Image.frombytes(mode, size, data)
+                    img.save(f'img{pgNum}.png')
+                elif xObject[obj]['/Filter'] == '/DCTDecode':
+                    img = open(join(workDir, f'img{pgNum}.jpg'), "wb")
+                    img.write(data)
+                    img.close()
+                elif xObject[obj]['/Filter'] == '/JPXDecode':
+                    img = open(join(workDir, f'img{pgNum}.jp2'), "wb")
+                    img.write(data)
+                    img.close()
+
+
+if __name__ == '__main__':
+    extractImages()
+
+print('Pages extracted')
