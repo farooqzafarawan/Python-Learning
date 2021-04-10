de60ab85697bbd5841f28abac20a6d06e0160dbe
mohibalvi <mohib.tech@gmail.com>
2021-04-10 14:00:55 +0500

    added CCITTFaxDecode filter for tiff

--- FileHandling/PDF/PDF_to_imgs.py
+++ FileHandling/PDF/PDF_to_imgs.py
@@ -1,17 +1,29 @@
 from PyPDF4 import PdfFileReader,PdfFileWriter
 from os.path import join
-#import PyPDF2
+import argparse
 from PIL import Image
+import io
 
 workDir = r'C:\URDU\eBooks\PDF books\MISC'
-fileName,ext = 'Laal Qilay ky Shaam-e-Sahar', '.pdf'
-filePDF = join(workDir, fileName+ext)
+#fileName = 'Laal Qilay ky Shaam-e-Sahar'
+fileName = 'Siraab Maghrib'
+a = 'سراب مغرب'
+ext = '.pdf'
 
 jpg = '.jpg'
 jp2 = '.jp2'
 png = '.png'
+tiff = '.tiff'
+
 
 def extractImages():
+    # parser = argparse.ArgumentParser(prog='PDFtoImage',description='Extracting images from PDF')
+    # parser.add_argument('infile', help='Enter name for input source PDF file')
+    # args = parser.parse_args()
+    # fileName= args.infile
+
+    filePDF = join(workDir, fileName+ext)
+
     pdfReader = PdfFileReader(open(filePDF, "rb"))
 
     for pgNum in range(pdfReader.numPages):
@@ -27,16 +39,24 @@ def extractImages():
                 else:
                     mode = "P"
 
+
                 if xObject[obj]['/Filter'] == '/FlateDecode':
                     img = Image.frombytes(mode, size, data)
                     outFile = join(workDir, f'{pgNum}_{fileName}{png}')
                     img.save(outFile)
                 elif xObject[obj]['/Filter'] == '/DCTDecode':
-                    img = open(join(workDir, f'{pgNum} {fileName}{jpg}'), "wb")
-                    img.write(data)
-                    img.close()
+                    img = Image.open(io.BytesIO(data))
+                    outFile = join(workDir, f'{pgNum}_{fileName}{jpg}')
+                    img.save(outFile)
+                    # img = open(join(workDir, f'{pgNum} {fileName}{jpg}'), "wb")
+                    # img.write(data)
+                    # img.close()
                 elif xObject[obj]['/Filter'] == '/JPXDecode':
                     img = open(join(workDir, f'{pgNum} {fileName}{jp2}'), "wb")
+                    img.write(data)
+                    img.close()
+                elif xObject[obj]['/Filter'] == '/CCITTFaxDecode':
+                    img = open(join(workDir, f'{pgNum} {fileName}{tiff}'), "wb")
                     img.write(data)
                     img.close()
 
