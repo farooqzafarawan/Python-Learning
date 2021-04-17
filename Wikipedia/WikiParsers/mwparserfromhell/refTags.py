433306f117e0f7c28bd716a2bf0a65c3c288b1bc
mohibalvi <mohib.tech@gmail.com>
2021-04-17 19:22:02 +0500

    extract ref tags from wikicode

---
+++ Wikipedia/WikiParsers/mwparserfromhell/refTags.py
@@ -0,0 +1,19 @@
+import mwparserfromhell as mwp
+from pathlib import Path
+
+BASE_DIR = Path(r'D:\script\MohibPython\DATA')
+infile = 'انیٹ فیلوز.txt'
+wikifile = BASE_DIR / infile
+
+with open(wikifile, encoding='utf-8') as f:
+    wikitext = f.read()
+
+wikicode = mwp.parse(wikitext)
+
+
+tags = []
+for tag in wikicode.filter_tags(matches=lambda tag: tag.tag == "ref"):
+        tags.append(tag)
+
+for tag in tags:
+    print(tag)
