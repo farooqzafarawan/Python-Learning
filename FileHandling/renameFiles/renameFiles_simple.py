038d2987110679537c626c60d7b87db113cbd78a
farooqzafarawan <farooqzafar@outlook.com>
2024-06-01 17:53:11 +0500

    moved to renameFiles folder

---
+++ FileHandling/renameFiles/renameFiles_simple.py
@@ -0,0 +1,25 @@
+import os
+from tkinter import filedialog , Tk
+
+tkroot = Tk()  # Initializing Tkinter
+workingDir = filedialog.askdirectory(parent=tkroot, initialdir="/", title='Please select a directory')
+
+#workingDir = r'C:\MEDIA\TV\MrRobot'
+
+splitChar = '.'
+#secondsplit = ''
+
+
+# Traverse root directory, and list directories as dirs and files as files
+for root, _, fileList in os.walk(workingDir):
+    path = root.split(os.sep)
+
+    for fname in fileList:
+        fname, ext = os.path.splitext(fname)
+
+        mainName = fname.split(splitChar)  # main file name
+        print(mainName)
+        newname = str(mainName[2]) + '_' + str(mainName[1]) + ext
+        lenpath = len(path) * '..'
+        print(f'{lenpath} {newname}')
+
