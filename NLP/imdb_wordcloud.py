22c5d6444ba5bd9d1b25eb3d453ac9b3e01b7a11
farooqzafarawan <farooqza.lhr@gmail.com>
2025-08-20 22:47:13 +0500

    read csv file and print df.head

--- NLP/imdb_wordcloud.py
+++ NLP/imdb_wordcloud.py
@@ -1,3 +1,16 @@
 #  IMDB dataset and this dataset contains 50,000 movie reviews in CSV format.
 # https://media.geeksforgeeks.org/wp-content/uploads/20250228115748017361/IMDB-Dataset.csv
+from pathlib import Path
+import pandas as pd
 
+working_dir = Path(r"D:\CODE\DATA")
+datafile = working_dir / "IMDB-Dataset.csv"
+
+df = pd.read_csv(datafile)
+
+print(df.head())
+
+# Step 2: Understanding the Dataset
+# The dataset contains two columns: 
+# review: Contains the movie review text
+# sentiment: It shows whether the review is positive or negative
